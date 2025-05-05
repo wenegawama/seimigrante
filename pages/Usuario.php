7ae9bed2f19615f9b865063169e79daa72c49a80
Wenega Wama <narcissewatherloy@gmail.com>
2025-05-05 01:00:00 -0300

    Cadastro do usuario feito com sucesso

---
+++ pages/Usuario.php
@@ -0,0 +1,95 @@
+<?php
+
+class Usuario {
+    private $id;
+    private $nome;
+    private $celular;
+    private $genero;
+    private $pais;
+    private $email_login;
+    private $senha;
+    private $perfil;
+    private $ativo;
+
+    public function __construct($id, $nome, $celular, $genero, $pais, $email_login, $senha, $perfil, $ativo) {
+        $this->id = $id;
+        $this->nome = $nome;
+        $this->celular = $celular;
+        $this->genero = $genero;
+        $this->pais = $pais;
+        $this->email_login = $email_login;
+        $this->setSenha($senha); 
+        $this->perfil = $perfil;
+        $this->ativo = $ativo;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function getCelular() {
+        return $this->celular;
+    }
+
+    public function getGenero() {
+        return $this->genero;
+    }
+
+    public function getPais() {
+        return $this->pais;
+    }
+
+    public function getEmailLogin() {
+        return $this->email_login;
+    }
+
+    public function getSenha() {
+        return $this->senha;
+    }
+
+    public function getPerfil() {
+        return $this->perfil;
+    }
+
+    public function isAtivo() {
+        return $this->ativo;
+    }
+
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    public function setCelular($celular) {
+        $this->celular = $celular;
+    }
+
+    public function setGenero($genero) {
+        $this->genero = $genero;
+    }
+
+    public function setPais($pais) {
+        $this->pais = $pais;
+    }
+
+    public function setEmailLogin($email_login) {
+        $this->email_login = $email_login;
+    }
+
+    public function setSenha($senha) {
+        $this->senha = password_hash($senha, PASSWORD_DEFAULT);
+    }
+
+    public function setPerfil($perfil) {
+        $this->perfil = $perfil;
+    }
+
+    public function setAtivo($ativo) {
+        $this->ativo = $ativo;
+    }
+}
+
+?>
